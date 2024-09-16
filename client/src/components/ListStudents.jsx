cf816e97e81a7e9d45b8dea2a06ff0d89169472b
stmcpeters <stmcpeters@gmail.com>
2024-09-15 19:44:51 -0700

    edits file to match events data

--- client/src/components/ListStudents.jsx
+++ client/src/components/ListStudents.jsx
@@ -1,59 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import * as ioicons from 'react-icons/io5'
 import MyForm from './Form';
-import Student from './Student';
+import Event from './Event';
 
-const ListStudents = () => {
+const ListEvents = () => {
 
-    // this is my original state with an array of students 
-    const [students, setStudents] = useState([]);
+    // this is my original state with an array of events 
+    const [events, setEvents] = useState([]);
 
     //this is the state needed for the UpdateRequest
-    const [editingStudent, setEditingStudent] = useState(null)
+    const [editingEvent, setEditingEvent] = useState(null)
 
-    const loadStudents = () => {
-        // A function to fetch the list of students that will be load anytime that list change
-        fetch("http://localhost:8080/api/students")
+    const loadEvents = () => {
+        // A function to fetch the list of events that will be load anytime that list change
+        fetch("http://localhost:8080/api/events")
             .then((response) => response.json())
             .then((students) => {
-                setStudents(students);
+                setEvents(events);
             });
     }
 
     useEffect(() => {
-        loadStudents();
-    }, [students]);
+        loadEvents();
+    }, [events]);
 
-    const onSaveStudent = (newStudent) => {
-        //console.log(newStudent, "From the parent - List of Students");
-        setStudents((students) => [...students, newStudent]);
+    const onSaveEvent = (newEvent) => {
+        //console.log(newEvent, "From the parent - List of Events");
+        setEvents((events) => [...events, newEvent]);
     }
 
 
-    //A function to control the update in the parent (student component)
-    const updateStudent = (savedStudent) => {
-        // console.log("Line 29 savedStudent", savedStudent);
-        // This function should update the whole list of students - 
-        loadStudents();
+    //A function to control the update in the parent (event component)
+    const updateEvent = (savedEvent) => {
+        // console.log("Line 29 savedEvent", savedEvent);
+        // This function should update the whole list of events - 
+        loadEvents();
     }
 
     //A function to handle the Delete funtionality
-    const onDelete = (student) => {
-        //console.log(student, "delete method")
-        return fetch(`http://localhost:8080/api/students/${student.id}`, {
+    const onDelete = (event) => {
+        //console.log(event, "delete method")
+        return fetch(`http://localhost:8080/api/events/${event.id}`, {
             method: "DELETE"
         }).then((response) => {
             //console.log(response);
             if (response.ok) {
-                loadStudents();
+                loadEvents();
             }
         })
     }
 
     //A function to handle the Update functionality
-    const onUpdate = (toUpdateStudent) => {
-        //console.log(toUpdateStudent);
-        setEditingStudent(toUpdateStudent);
+    const onUpdate = (toUpdateEvent) => {
+        //console.log(toUpdateEvent);
+        setEditingEvent(toUpdateEvent);
 
     }
 
@@ -61,18 +61,18 @@ const ListStudents = () => {
 
     return (
         <div className="mybody">
-        <div className="list-students">
-            <h2>Techtonica Participants </h2>
+        <div className="list-events">
+            <h2>Local Events Near You</h2>
             <ul>
-                {students.map((student) => {
-                    return <li key={student.id}> <Student student={student} toDelete={onDelete} toUpdate={onUpdate} /></li>
+                {events.map((event) => {
+                    return <li key={event.id}> <Event event={event} toDelete={onDelete} toUpdate={onUpdate} /></li>
                 })}
             </ul>
         </div>
-        <MyForm key={editingStudent ? editingStudent.id : null} onSaveStudent={onSaveStudent} editingStudent={editingStudent} onUpdateStudent={updateStudent} />
+        <MyForm key={editingEvent ? editingEvent.id : null} onSaveEvent={onSaveEvent} editingEvent={editingEvent} onUpdateEvent={updateEvent} />
         </div>
     );
 }
 
 
-export default ListStudents
+export default ListEvents
